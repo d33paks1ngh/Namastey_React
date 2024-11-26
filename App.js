25fa574d735707af2814a0af73578e8fe23d83e7
Deepak Singh <ds8853845715@gmail.com>
2024-11-26 23:54:00 +0530

    Episode_04

--- App.js
+++ App.js
@@ -1,30 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headtag = <h1>hello from jsx</h1>;
+/**
+ * applayout
+ *
+ *
+ *
+ *
+ */
 
-const HeadingComponent = () => {
-  return <h1>hello from HeadingComponent</h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://image.similarpng.com/very-thumbnail/2020/08/Fresh-food-logo-design-on-transparent-background-PNG.png"
+        />
+      </div>
+
+      <div className="nav-items">
+        <ul className="list-items">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Card</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const TitleComponent = () => <h1>Hello React from TitleComponent</h1>;
+const RestaurantCard = (props) => {
+  // console.log(props);
+  const {name,cousines}=props
 
-const element = (
-  <div>
-    {headtag}
-    <HeadingComponent />
-    <TitleComponent />
-    <h1>Hello from jsx element</h1>
-  </div>
-);
+  return (
+    <div className="res-card">
+      <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597" />
+      <h3>{name}</h3>
+      <h4>{cousines}</h4>
+    </div>
+  );
+};
 
-const BodyComponent = () => (
-  <div id="container">
-    {headtag}
-    <h1>hello from BodyComponent</h1>
-    <TitleComponent />
-  </div>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <input /> search
+      </div>
+
+      <div className="res-container">
+        <RestaurantCard name="Meghana Food" cousines="Murga Biryani Non Veg" />
+        <RestaurantCard name="KFC" cousines="Coffee Food Tea" />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<AppLayout />);
--- readme.md
+++ readme.md
@@ -1,26 +1,48 @@
-#Namastey_React 
-episode 01- learning about createReactElement , ReactDom.createRoot, and root.render();
- 
-#Episode_02 - 
+#Namastey_React
+
+# Episode_01- Inception
+
+- learning about createReactElement
+- ReactDom.createRoot
+- root.render();
+
+# Episode_02 Igniting our app
+
 # Parcel
--dev build 
+
+-dev build
+
 - Local server
--HMR = hot module Replacement
+  -HMR = hot module Replacement
 - File watching Algorithm written in cpp
 - Caching = Faster Builds
--Image optimization
--Bundling
--Minification in the production build
--Compressing
+  -Image optimization
+  -Bundling
+  -Minification in the production build
+  -Compressing
 - Consistent Hashing
--Code Splitting
+  -Code Splitting
 - Differential Bundling
 - Diagostic
 - Error Handing
 - Tree Shaking - remove unused code
 
 # Episode_03 laying the foundation
+
 -learning about jsx
 -Type of component in React
 -Functional Component
 -jsx->converted into react.createElement->react Element->js object->render=HTML code on the browser.
+
+# Episode_04 Talk is cheap show me the code
+
+- planning to create food ordering app
+- AppLayout
+  header -> Logo and Nav
+  body -> search bar and Restaurant Container
+  Restaurant Container-> Res-card
+- Learn about Props
+- Passing props and learning about API
+- Config driven UI
+- Working On real swiggy API
+- UI is powered by Data
